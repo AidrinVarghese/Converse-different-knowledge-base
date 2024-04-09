3955588ee1ac956a6c33583b979de7f21a48971d
Aidrin Varghese <aidrinv2001@gmail.com>
2024-04-09 21:24:44 +0530

    feat: Login page UI completed

--- register_page.py
+++ register_page.py
@@ -1,6 +1,10 @@
 from config.streamlit_config import authenticator
+import streamlit as st
 
 # authenticator.login()
+if st.button("Go back", type="secondary"):
+    st.session_state.logged_in = False
+    st.switch_page("starter.py")
 
 try:
     email_of_registered_user, username_of_registered_user, name_of_registered_user = (
@@ -8,6 +12,7 @@ try:
     )
     if email_of_registered_user:
         # st.success("User registered successfully")
-        st.page_link("pages/login_page.py", label="Login Page")
+        st.page_link("login_page.py", label="Login Page")
 except Exception as e:
     st.error(e)
+
