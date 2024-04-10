07b4b9959806c5ba147593f98efea2fbf599fe34
Aidrin Varghese <aidrinv2001@gmail.com>
2024-04-10 11:13:32 +0530

    fix: Chatbot page and feat: Add chat history

--- pages/document_chat_page.py
+++ pages/document_chat_page.py
@@ -41,7 +41,7 @@ def get_vector_store(chunks):
 def get_conversational_chain():
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    provided context just say, "answer is not available in the context", don't provide the wrong answer. Also instead of using your name ie Gemini please use the name Converse if asked\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
